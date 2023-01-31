452238f112b0036ae7e2831ff94c512a40748a41
Code Sandbox <hello@codesandbox.io>
2023-01-31 03:35:38 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c098b4c52e0638b7e70683f38c42a0d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4bd234c8ce82fab5ab23037ddbdeaf",
+    "url": "https://sids4ru.github.io/csb-95xpm3/index.html"
+  },
+  {
+    "revision": "84739979eac4bbec3651",
+    "url": "https://sids4ru.github.io/csb-95xpm3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "45ea85f263667c45c435",
+    "url": "https://sids4ru.github.io/csb-95xpm3/static/js/2.027cbeee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://sids4ru.github.io/csb-95xpm3/static/js/2.027cbeee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84739979eac4bbec3651",
+    "url": "https://sids4ru.github.io/csb-95xpm3/static/js/main.0773039c.chunk.js"
+  },
+  {
+    "revision": "dc1dcae43b102a0b0dd8",
+    "url": "https://sids4ru.github.io/csb-95xpm3/static/js/runtime-main.c8d3359e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://sids4ru.github.io/csb-95xpm3/static/media/logo.5d5d9eef.svg"
+  }
+]);
